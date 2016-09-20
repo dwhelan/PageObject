36b73dec86c75154c7a1a3d93ca5b3b3401805c0
dwhelan <dwhelan@leanintuit.com>
2016-09-20 01:06:42 -0700

    Updated test to set environment variables rather than use current directory in order to simplify the tests.

--- PageObject.Tests/PageConstruction/WithEnvironmentVariables.cs
+++ PageObject.Tests/PageConstruction/WithEnvironmentVariables.cs
@@ -6,7 +6,19 @@ namespace PageObject.Tests.PageConstruction
     [TestFixture]
     public class WithEnvironmentVariables : BaseTest
     {
-        private static readonly string CurrentDirectoryPath = Environment.CurrentDirectory.Replace('\\', '/');
+        [TestFixtureSetUp]
+        public void SetEnvironmentVariables()
+        {
+            Environment.SetEnvironmentVariable("<host>", "host");
+            Environment.SetEnvironmentVariable("<path>", "path");
+        }
+
+        [TestFixtureTearDown]
+        public void RemoveEnvironmentVariables()
+        {
+            Environment.SetEnvironmentVariable("<host>", null);
+            Environment.SetEnvironmentVariable("<path>", null);
+        }
 
         [TestCase(typeof(PageWithEnvironmentVariableInPath))]
         [TestCase(typeof(PageWithEnvironmentVariableInUrl))]
@@ -14,21 +26,21 @@ namespace PageObject.Tests.PageConstruction
         public void Should_expand_environment_variable_in_path(Type pageClass)
         {
             var page = CreatePage(pageClass);
-            Assert.That(page.Url, Is.EqualTo(string.Format("file:///{0}/", CurrentDirectoryPath)));
+            Assert.That(page.Url, Is.EqualTo("http://host/path"));
         }
-            [PageAt("file:///{cd}/")]
+            [PageAt("http://{<host>}/{<path>}")]
             private class PageWithEnvironmentVariableInPath : Page
             {
                 public PageWithEnvironmentVariableInPath(PageSession session) : base(session) { }
             }
 
-            [PageAt("file:///{CD}/", "")]
+            [PageAt("http://{<host>}/{<path>}", "")]
             private class PageWithEnvironmentVariableInUrl : Page
             {
                 public PageWithEnvironmentVariableInUrl(PageSession session) : base(session) { }
             }
 
-            [PageAt("file:///", "{CD}/")]
+            [PageAt("http://{<host>}", "{<path>}")]
             private class PageWithEnvironmentVariableInUrlAndPath : Page
             {
                 public PageWithEnvironmentVariableInUrlAndPath(PageSession session) : base(session) { }
