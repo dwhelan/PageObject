c0f6f7bb7d6a02c2bec67bc7209b3637d37471fe
dwhelan <dwhelan@leanintuit.com>
2016-09-21 23:22:17 -0700

    Refactored tests for OnPage functionality.

--- PageObject.Tests/OnPageTests.cs
+++ PageObject.Tests/OnPageTests.cs
@@ -11,80 +11,49 @@ namespace PageObject.Tests
         public HomePage(PageSession session) : base(session) {}
     }
 
-    [PageAt("file:///${cd}/../../Pages/File/Home.html")]
-    internal class HomePage2 : Page
-    {
-        public HomePage2(PageSession session) : base(session) { }
-    }
-
-    [PageAt("file://localhost/${cd}/../../Pages/File/Home.html", HostMatch = ".*")]
-    internal class HomePage3 : Page
-    {
-        public HomePage3(PageSession session) : base(session) { }
-    }
-
-    [PageAt("file:///${cd}/../../Pages/File2/Home.html", PathMatch = @"Z:/code/cs/PageObject/PageObject.Tests/Pages/File\d?/Home.html")]
-    internal class HomePage4 : Page
-    {
-        public HomePage4(PageSession session) : base(session)
-        {
-        }
-    }
-
     [TestFixture]
     public class OnPageTests
     {
-        private SessionConfiguration configuration;
         private PageSession session;
-        private Page page;
 
         [TestFixtureSetUp]
         public void VisitTheHomePage()
         {
-            configuration = new SessionConfiguration { Browser = Browser.PhantomJS };
+            var configuration = new SessionConfiguration { Browser = Browser.PhantomJS };
             session = new PageSession(configuration);
-            page = new HomePage(session);
-            page.Visit();
+            new HomePage(session).Visit();
         }
 
-        [Test]
-        public void Page_should_be_active_when_visited()
+        [TestCase(typeof(HomePage))]
+        [TestCase(typeof(PageWithSameUrl))]
+        [TestCase(typeof(PageWithMatchingHostAndSamePath))]
+        [TestCase(typeof(PageWithSameHostAndMatchingPath))]
+        public void Should_be_on_matching_pages(Type pageClass)
         {
-            Assert.That(page.IsActive);
-        }
-
-        [Test]
-        public void Another_instance_of_the_page_should_be_active_when_visited()
-        {
-            var anotherPage = new HomePage(session);
+            var anotherPage = PageFactory.Instance.PageFor(pageClass, session);
 
             Assert.That(anotherPage.IsActive);
         }
 
-        [Test]
-        public void Another_page_with_the_same_url_should_be_active_when_visited()
-        {
-            var anotherPage = new HomePage2(session);
-        
-            Assert.That(anotherPage.IsActive);
-        }
+            [PageAt("file:///${cd}/../../Pages/File/Home.html")]
+            internal class PageWithSameUrl : Page
+            {
+                public PageWithSameUrl(PageSession session) : base(session) { }
+            }
 
-        [Test]
-        public void Another_page_with_same_path_and_matching_host_should_be_active()
-        {
-            var anotherPage = new HomePage3(session);
-        
-            Assert.That(anotherPage.IsActive);
-        }
+            [PageAt("file://localhost/${cd}/../../Pages/File/Home.html", HostMatch = ".*")]
+            internal class PageWithMatchingHostAndSamePath : Page
+            {
+                public PageWithMatchingHostAndSamePath(PageSession session) : base(session) { }
+            }
 
-        [Test]
-        public void Another_page_with_a_matching_path_should_be_active()
-        {
-            var anotherPage = new HomePage4(session);
-        
-            Assert.That(anotherPage.IsActive);
-        }
-
+            [PageAt("file:///${cd}/../../Pages/File2/Home.html", PathMatch = @"Z:/code/cs/PageObject/PageObject.Tests/Pages/File\d?/Home.html")]
+            internal class PageWithSameHostAndMatchingPath : Page
+            {
+                public PageWithSameHostAndMatchingPath(PageSession session) : base(session)
+                {
+                }
+            }
         [TestFixtureTearDown]
         public void DisposeSession()
         {
