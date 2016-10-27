e9887a03bd8568ea30e0922266464d4ba36d7230
dwhelan <dwhelan@leanintuit.com>
2016-10-26 23:03:59 -0400

    R: Simplified parameters to ElementAttributeFor().

--- PageObject/Elements/ElementFactory.cs
+++ PageObject/Elements/ElementFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 namespace PageObject.Elements
 {
@@ -6,13 +7,12 @@ namespace PageObject.Elements
     {
         public static T ElementFor<T>(Page page, string propertyName) where T : BaseElement
         {
-            var attribute = PropertyAttribute(page.GetType(), propertyName);
+            var attribute = ElementAttributeFor(page.GetType().GetProperty(propertyName));
             return (T) Activator.CreateInstance(typeof(T), attribute, page.Browser);
         }
 
-        private static ElementAttribute PropertyAttribute(Type type, string name)
+        private static ElementAttribute ElementAttributeFor(PropertyInfo property)
         {
-            var property = type.GetProperty(name);
             return (ElementAttribute) property.GetCustomAttributes(typeof(ElementAttribute), true)[0];
         }
     }
